f71792d8eff3f580ccc20ef63b8ab37f8b1d6e77
maliyan <ahmadmaliyan@gmail.com>
2019-04-30 21:05:59 +0700

    add File

---
+++ purwadhika.py
@@ -0,0 +1,30 @@
+# print('Hello Word!!!')
+# nama = 'Andi';
+# print(nama);
+# usia = 22;
+# usia = 32;
+# print(usia);
+# jomblo = True;
+# print(jomblo);
+
+# print(type(nama));
+# print(type(usia));
+# print(type(jomblo));
+
+# contoh = 343.5; 
+# print(type(contoh));
+
+# print(5*'Andi'); #membuat duplikat
+
+
+
+
+nama = input("Nama kamu? : ");
+umur = input("Umur kamu? : ");
+kelamin = input("Kelamin kamu? : ");
+pekerjaan = input("Pekerjaan kamu? : ");
+
+print("Nama : " + nama);
+print("Umur : " + umur);
+print("Kelamin : " + kelamin);
+print("Pekerjaan : " + pekerjaan);
